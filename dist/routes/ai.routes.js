7d3b8082ffd405fbde5344bfa16edbf07058d328
Harsh Gaur <hgaur491@gmail.com>
2024-11-30 11:43:30 +0530

    vercel testing with ts

--- dist/routes/ai.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ai_controller_1 = require("../controllers/ai.controller");
-const aiRouter = (0, express_1.Router)();
-aiRouter.route("/completion").post(ai_controller_1.completion);
-exports.default = aiRouter;
