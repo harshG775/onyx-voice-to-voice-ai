a81dc45288aadc03767b5a2a21e141734338082f
harshG775 <hgaur491@gmail.com>
2025-07-27 17:34:53 +0530

    //

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from "url";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -17,5 +18,10 @@ export default defineConfig({
     server: {
         port: 4000,
         host: true,
+        https: {
+            //NOTE: mkcert localhost to generate the certificates
+            key: fs.readFileSync("./localhost-key.pem"),
+            cert: fs.readFileSync("./localhost.pem"),
+        },
     },
 });
